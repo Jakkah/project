52710556d984ba0017e7f2dd5418a7da1a56feab
Jakkah <jakah.cgi@gmail.com>
2021-01-20 18:13:46 +0100

    Fix async request dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AuthService from "../services/auth.service";
-import { Jumbotron, Button, ListGroup, ListGroupItem } from "reactstrap";
+import {
+  Jumbotron,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  CardImg,
+  Input,
+  Form,
+} from "reactstrap";
 import "./Dashboard.css";
 
 const createHistory = require("history").createBrowserHistory;
@@ -9,64 +17,91 @@ const history = createHistory();
 
 const Dashboard = () => {
   const currentUser = AuthService.getCurrentUser().user;
-  const [showIt, setShowIt] = useState(false);
-  const update = () => {
-    setShowIt(!showIt);
-    console.log(showIt);
-  };
+  const API_URL = "http://localhost:5000/api/users/" + currentUser._id;
   if (!currentUser) {
     history.push("/");
     window.location.reload();
   }
-  const liste = Object.values(currentUser.skills);
-  // useEffect(() => {
-  //   const API_URL = "http://localhost:5000/api/users/" + currentUser.id;
 
-  //   const fetchData = async () => {
-  //     const result = await axios(API_URL);
+  const [image, ChangeImage] = useState(
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  );
+  const [showIt, setShowIt] = useState(false);
+  const [newSkill, setNewSkill] = useState("");
+  const [data, setData] = useState();
+  const [Loading, setLoading] = useState(true);
+  const [ListSkill, setListSkill] = useState();
 
-  //     setData(result.data);
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(API_URL);
+      setData(result.data);
+      setListSkill(Object.values(result.data.skills));
+      console.log(Loading);
+      setLoading(false);
+    };
+    fetchData();
+  }, [Loading]);
 
-  //   fetchData();
-  // }, []);
+  const update = () => {
+    setShowIt(!showIt);
+    console.log(showIt);
+  };
+
+  const handleNewSkill = (e) => {
+    setNewSkill(e.target.value);
+  };
+
+  const AddSkill = (e) => {
+    e.preventDefault();
+    ListSkill.push(newSkill);
+    console.log(ListSkill);
+    setLoading(true);
+  };
+  const Render = () => {
+    return (
+      <div>
+        <h1>Tableau de bord</h1>
+        <Jumbotron>
+          <CardImg
+            top
+            src={image}
+            className="pull-right float-right img-fluid d-none d-sm-block image"
+            alt="profil_image"
+          />
+          <div>
+            <h3>{data.username}</h3>
+            <h6>{data.job}</h6>
+          </div>
+          <p>{data.email}</p>
+          <p>{data.type}</p>
+        </Jumbotron>
+
+        <Button className="update float-right" onClick={update}>
+          Update
+        </Button>
+        <ListGroup>
+          <h4>Compétences</h4>
+          {ListSkill.map((skill, index) => {
+            return <ListGroupItem key={index}>{skill}</ListGroupItem>;
+          })}
+        </ListGroup>
+        <Form className="col-12" onSubmit={AddSkill}>
+          <Input
+            type="text"
+            name="newSkill"
+            id="newSkill"
+            placeholder="Ajouter une compétence"
+            onChange={handleNewSkill}
+          />
+          <Button> Ajouter </Button>
+        </Form>
+      </div>
+    );
+  };
 
   return (
-    <div className="container">
-      <h1>Tableau de bord</h1>
-      <Jumbotron>
-        <Button className="update" onClick={update}>
-          X
-        </Button>
-        {showIt ? (
-          <div>
-            <input value={currentUser.username} />
-          </div>
-        ) : (
-          <h3>{currentUser.username}</h3>
-        )}
-        {showIt ? (
-          <div>
-            <input value={currentUser.email} />
-          </div>
-        ) : (
-          <p>{currentUser.email}</p>
-        )}
-        {showIt ? (
-          <div>
-            <input value={currentUser.type} />
-          </div>
-        ) : (
-          <p>{currentUser.type}</p>
-        )}
-      </Jumbotron>
-      <ListGroup>
-        <h4>Compétences</h4>
-        {liste.map((skill, index) => {
-          return <ListGroupItem key={index}>{skill}</ListGroupItem>;
-        })}
-      </ListGroup>
-    </div>
+    <div className="container">{Loading ? <h1>Loading</h1> : <Render />}</div>
   );
 };
 
